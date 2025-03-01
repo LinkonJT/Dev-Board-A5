962a1fca7129cc040d0903bf88019f408e0f54bf
LinkonJT <linkontripura@gmail.com>
2025-03-02 00:46:51 +0600

    number plus minus, clear history

--- javascript/front.js
+++ javascript/front.js
@@ -23,7 +23,7 @@ function updateDate() {
 
 updateDate();
 
-//completed-btn
+/*************completed-btn**********/
 
 const completedButtons = document.querySelectorAll(".completed-btn");
 
@@ -31,14 +31,29 @@ for(const button of completedButtons){
   button.addEventListener("click", function(){
     alert ("Board updated successfully")
 
+    //add to top total
 
+    const taskDone = document.getElementById("task-done");
+    const convertedTaskDone = parseInt(taskDone.innerText);
+    const totalTaskDone = convertedTaskDone + 1;
+    taskDone.innerText = totalTaskDone;
+
+//remaining task
+
+const remainingTask = document.getElementById("remaining-task");
+    const ConvertedRemainingTask = parseInt(remainingTask.innerText);
+    const totalRemainingTask = ConvertedRemainingTask - 1;
+    remainingTask.innerText = totalRemainingTask;
+
+
+
+
+    //completed time update
     const completedTime = new Date().toLocaleTimeString("en-US",{
       hour: "2-digit",
       minute: "2-digit",
       hour12: true
-
     })
-
     const activityContainer = document.getElementById("activity-container")
     const div = document.createElement("div");
     div.innerHTML = `<h2 class="bg-slate-200 rounded-sm text-xs p-4 mb-4">You have completed the task at ${completedTime }</h2>`
@@ -47,12 +62,11 @@ for(const button of completedButtons){
   })
 }
 
-// clear history
-document.getElementById("clear-history").addEventListener("click", function(){
-  
-  const activityContainer = document.getElementById("activity-container");
 
-  activityContainer.innerHTML = " "
+/************ clear history ***********/
+document.getElementById("clear-history").addEventListener("click", function(){
+  const activityContainer = document.getElementById("activity-container");
+  activityContainer.replaceChildren();
   
 })
 
